2d93aa23d174f7afa7b677b8547798ee8adb6792
MeepCastana <tmihai4597@gmail.com>
2024-12-14 21:59:34 +0200

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>BaiMeep + Sandu spedition</h1>
+      <h1>BaiMeep + Sandu</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
